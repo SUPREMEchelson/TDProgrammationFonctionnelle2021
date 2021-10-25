6c41f5d732cd45f4610fa238ea9058f03033dcca
SUPREMEchelson <chelson.sm@gmail.com>
2021-10-25 17:09:15 +0200

    exercice 2

--- src/main/java/Td2/Exercice2/App.java
+++ src/main/java/Td2/Exercice2/App.java
@@ -8,11 +8,23 @@ public class App {
 
         Pair<Integer,Double> Mathieu = new Pair<>(70,170.0);
 
-        Predicate<Pair> tropPetit = x -> x.fst<100;
-        Predicate<Pair> tropGrand = x ->x.snd>200.0;
+        Predicate<Integer> tailleTropPetit = taille -> taille <100;
+        Predicate<Integer> tailleTropGrand = taille -> taille >200;
+        Predicate<Integer> tailleIncorecte = tailleTropGrand.or(tailleTropPetit);
+        Predicate<Integer> tailleCorrecte = tailleIncorecte.negate();
 
-        System.out.println(tropPetit.test(Mathieu));
+        Predicate<Integer> poidsTropLourd = poids -> poids >150;
+        Predicate<Integer> poidsCorrecte = poidsTropLourd.negate();
 
+        Predicate<Integer> accessAutoriser = tailleCorrecte.or(poidsCorrecte);
+
+
+        System.out.println(poidsCorrecte.test(100));
+        System.out.println(poidsTropLourd.test(210));
+        System.out.println(tailleCorrecte.test(180));
+        System.out.println(tailleTropGrand.test(210));
+        System.out.println(tailleTropPetit.test(80));
+        System.out.println(tailleIncorecte.test(60));
 
 
 
---
+++ src/test/java/Td2/Exercice2/AppTest.java
@@ -0,0 +1,13 @@
+package Td2.Exercice2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    Pair<Integer,Double> Mathieu = new Pair<>(70,170.0);
+
+    void testTropGrand(){
+
+    }
+
+}
